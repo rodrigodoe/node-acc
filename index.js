997104c393201e13c92e6d718bb0a59beedac8de
Rodrigo Carvalho <roda7x@gmail.com>
2020-10-03 17:03:18 -0300

    corrigindo cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 const calculatorRoutes = require("./routes");
 
 const PORT = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api", calculatorRoutes);
