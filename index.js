1aa963fdfaffc05b230e398f55f4a517f5a92e14
Rodrigo Carvalho <roda7x@gmail.com>
2020-10-03 17:05:59 -0300

    corrigindo response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 const calculatorRoutes = require("./routes");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { convert } = require("../services");
 router.get("/", (req, res) => {
   const { number } = req.body;
   if (!number) {
-    res.status(400).send("Por favor, um numero deve ser informado");
+    return res.status(400).send("Por favor, um numero deve ser informado");
   }
 
   const letter = convert(number);
